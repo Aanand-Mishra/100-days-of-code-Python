3062e85f8ef616510eaf2f12a1e0072cd752b1de
Aanand Mishra <100397935+Aanand-Mishra@users.noreply.github.com>
2022-02-25 20:35:57 -0800

    Added the solution for Day 1

---
+++ Day 1/band name generator.py
@@ -0,0 +1,8 @@
+# Project - Band Name Generator
+print('Welcome To Band Name Generator')
+
+# .title() for proper output
+homeTown = input('Which city did you grow up in? \n').title()
+petName = input('What is the name of your pet? \n').title()
+
+print('Your Band Name Could Be '+ homeTown +' '+ petName)
---
+++ Day 1/excercises.py
@@ -0,0 +1,32 @@
+# Excercise 1 - Printing Challenge
+print('''Day 1 - Python Print Function
+The function is declared like this:''')
+print("print('what to print')")
+
+# Excercise 2 - Debug the Code👇
+'''
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+'''
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Excercise 3 - Character Counting
+userName = input('What is your name? ')
+totalCharacters = len(userName)
+print(totalCharacters)
+
+# Excercise 4 - Interchange the Value of Variables
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
